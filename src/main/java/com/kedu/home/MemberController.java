975d229bb0e6a8537e42cae4a3ae75146dc3571f
Xavier Choi <chaeins25@naver.com>
2025-04-01 12:25:05 +0900

    selectEmpforPermission add

--- src/main/java/com/kedu/home/MemberController.java
+++ src/main/java/com/kedu/home/MemberController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -54,5 +55,9 @@ public class MemberController {
 		
 		
 	}
+	@GetMapping("/selectEmpForPermission")
+    public List<MemberDTO> getAllEmployees() {
+        return mServ.getAllEmployees();
+}
 	
 }
