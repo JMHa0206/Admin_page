d553c02f4bd8fa8edc0bd242e17c1c7fb74036ca
jeokaet <whgnldud0325@naver.com>
2025-04-14 11:38:26 +0900

    jeokaet 힘들다;;

--- src/main/java/com/kedu/services/WorkService.java
+++ src/main/java/com/kedu/services/WorkService.java
@@ -9,7 +9,7 @@ import com.kedu.dao.WorkDAO;
 import com.kedu.dto.AnnualLeaveDTO;
 import com.kedu.dto.DepartDTO;
 import com.kedu.dto.MemberDTO;
-import com.kedu.dto.WorkDTO;
+import com.kedu.dto.WorkDisplayDTO;
 import com.kedu.dto.WorkingHoursDTO;
 
 @Service
@@ -18,9 +18,6 @@ public class WorkService {
     @Autowired
     private WorkDAO workDAO;
 
-    public void updateWorkTime(WorkDTO dto) {
-        workDAO.updateWorkStandard(dto);
-    }
 
     public void createAnnualLeaveRule(AnnualLeaveDTO dto) {
         workDAO.createAnnualLeave(dto);
@@ -29,7 +26,7 @@ public class WorkService {
     public List<AnnualLeaveDTO> selectAll(){
     	return workDAO.selectAll();
     }
-    
+    // 부서 출력
     public List<DepartDTO> selectAllDept(){
     	return workDAO.selectAllDept();
     }
@@ -39,6 +36,14 @@ public class WorkService {
     
     public int setWorkHours(WorkingHoursDTO whdto) {
     	return workDAO.setWorkHours(whdto);
+    }
+    // 부서별 출퇴근 시간 조회
+    public List<WorkDisplayDTO> getWorkHoursByDept(int deptId) {
+        return workDAO.getWorkHoursByDept(deptId);
+    }
+    // 부서 이름만 출력
+    public List<DepartDTO> getDeptListForDropdown() {
+        return workDAO.selectAllDeptForDropdown();
     }
 
 }
